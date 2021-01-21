f8cca7fdd84c7520fd082fc4b45a0742f257c8a4
Fozzy3 <carlmora027@gmail.com>
2021-01-21 15:54:03 -0500

    La validacion de campos funciona perfectamente

--- MCD/Form1.cs
+++ MCD/Form1.cs
@@ -42,5 +42,7 @@ namespace MCD
         {
             Application.Exit();
         }
+
+   
     }
 }
--- MCD/Form3.Designer.cs
+++ MCD/Form3.Designer.cs
@@ -81,7 +81,6 @@ namespace MCD
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(151, 34);
             this.textBox1.TabIndex = 16;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
